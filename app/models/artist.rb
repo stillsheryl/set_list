eb893789e1af8ee37b68b4d23995178b2bfc7863
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-09 16:44:47 -0600

    Add average song length method

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,12 @@
 class Artist < ApplicationRecord
   has_many :songs
-  
+
   validates_presence_of :name
+
+  def average_song_length
+    total = self.songs.sum do |song|
+      song.length
+    end
+    total/self.songs.count
+  end
 end
