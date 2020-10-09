16853d264a3de878e353e8b81a09ef2bf1e6c7f3
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-09 12:50:06 -0600

    Connect artist and songs in database

--- spec/features/songs/show_spec.rb
+++ spec/features/songs/show_spec.rb
@@ -8,7 +8,7 @@ describe "As a user" do
                            play_count:  453254)
 
       visit "/songs/#{song_1.id}"
-      save_and_open_page # use locally for testing and then remove
+
       expect(page).to have_content("#{song_1.title}")
       expect(page).to have_content("Length: #{song_1.length}")
       expect(page).to have_content("Play Count: #{song_1.play_count}")
