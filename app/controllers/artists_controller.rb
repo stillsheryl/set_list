fc11a6be5ade54b28b7039034f58a2e1b99ba452
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-09 18:43:57 -0600

    Add new artist functionality

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,17 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
   end
+
+  def new
+  end
+
+  def create
+    Artist.create(artist_params)
+    redirect_to '/artists'
+  end
+
+  private
+  def artist_params
+    params.permit(:name)
+  end
 end
