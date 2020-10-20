a80f2c18b28383824615d104d0038111fa5660c2
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-20 16:46:41 -0600

    Add before each test to songs index test

--- spec/features/songs/index_spec.rb
+++ spec/features/songs/index_spec.rb
@@ -2,38 +2,35 @@ require "rails_helper"
 
 describe "As a user" do
   describe "when I visit the /songs path" do
-    it "the user sees each song title and play count" do
-      artist_1 = Artist.create!(name: "Carly")
-      song_1 = artist_1.songs.create!(title:       "I Really Like You",
+    before :each do
+      @artist_1 = Artist.create!(name: "Carly")
+      @song_1 = @artist_1.songs.create!(title:       "I Really Like You",
                            length:      208,
                            play_count:  243810867)
-      song_2 = artist_1.songs.create!(title:       "Call Me Maybe",
+      @song_2 = @artist_1.songs.create!(title:       "Call Me Maybe",
                            length:      199,
                            play_count:  1214722172)
+    end
 
+    it "the user sees each song title and play count" do
       visit '/songs'
 
       within("#header-song") do
         expect(page).to have_content("Song List")
       end
 
-      # within '.songs' do
-      #   expect(page.all(li)[0]).to have_content("#{song_1.title}")
-      #   expect(page.all(li)[1]).to have_content("#{song_2.title}")
-      # end
-
-      within "#song-#{song_1.id}" do
-        expect(page).to have_content("#{song_1.title}")
-        expect(page).to have_content("Play Count: #{song_1.play_count}")
+      within "#song-#{@song_1.id}" do
+        expect(page).to have_content("#{@song_1.title}")
+        expect(page).to have_content("Play Count: #{@song_1.play_count}")
       end
 
       within all('.songs')[0] do
-        expect(page).to have_content("#{song_1.title}")
+        expect(page).to have_content("#{@song_1.title}")
       end
 
       within all('.songs')[1] do
-        expect(page).to have_content("#{song_2.title}")
-        expect(page).to have_content("Play Count: #{song_2.play_count}")
+        expect(page).to have_content("#{@song_2.title}")
+        expect(page).to have_content("Play Count: #{@song_2.play_count}")
       end
     end
   end
