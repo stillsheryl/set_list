171081046892a3f6d3c40b65272e0844debe1aae
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-22 10:03:31 -0600

    Add test user is logged in after creating an account

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -18,4 +18,22 @@ RSpec.describe "User registration form" do
 
     expect(page).to have_content("Welcome, #{username}!")
   end
+
+  it "keeps a user logged in after registering" do
+    visit '/'
+
+    click_on "Register as a User"
+
+    username = "funbucket13"
+    password = "test"
+
+    fill_in :username, with: username
+    fill_in :password, with: password
+
+    click_on "Create User"
+
+    visit '/profile'
+
+    expect(page).to have_content("Hello, #{username}!")
+  end
 end
