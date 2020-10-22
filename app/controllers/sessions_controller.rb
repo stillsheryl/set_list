f752ad5a00a27f829693f208399a9cdc27949c3c
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-22 11:06:35 -0600

    Add flash message for incorrect credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,13 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:username])
-    session[:user_id] = user.id
-    flash[:success] = "Welcome, #{user.username}!"
-    redirect_to '/'
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.username}!"
+      redirect_to '/'
+    else
+      flash.now[:notice] = "Sorry, your credentials are bad."
+      render :new
+    end
   end
 end
