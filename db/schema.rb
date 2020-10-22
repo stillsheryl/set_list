aeaa2cdd0b4b5c148657bfd313d89f17f67b22b3
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-22 09:50:32 -0600

    Add user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_004521) do
+ActiveRecord::Schema.define(version: 2020_10_22_154426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,11 @@ ActiveRecord::Schema.define(version: 2020_10_15_004521) do
     t.datetime "updated_at", null: false
     t.bigint "artist_id"
     t.index ["artist_id"], name: "index_songs_on_artist_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
   end
 
   add_foreign_key "playlist_songs", "playlists"
