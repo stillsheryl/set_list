b2e43564a483833e3e928bc7c8dc0c10430dc28c
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-26 15:41:32 -0600

    Add test default user doesn't see admin dashboard

--- spec/features/admin/email_spec.rb
+++ spec/features/admin/email_spec.rb
@@ -14,3 +14,18 @@ describe "Admin can email users" do
     end
   end
 end
+
+describe "as a default user" do
+  it "does not allow defauly user to see admin dashboard index" do
+    user = User.create(username: "fern@gully.com",
+                     password: "password",
+                     role: 0)
+
+    allow_any_instance_of(ApplicationController). to receive(:current_user).and_return(user)
+
+    visit '/admin/dashboard'
+
+    expect(page).to_not have_link("Email a User")
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+end
