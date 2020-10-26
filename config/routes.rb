f7883fb74a940511f2f7d029d2e8eb8ea75f7d40
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-26 15:17:22 -0600

    Update route and index page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post '/users', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
   get '/profile', to: 'users#show'
   get '/songs', to: 'songs#index'
   get '/songs/:id', to: 'songs#show'
